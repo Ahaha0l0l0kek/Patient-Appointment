38bccc554e9a59a0268ee77898707b3384385d29
Axelerator <lexabuchkov@gmail.com>
2023-01-30 21:21:59 +0300

    Initial commit

---
+++ src/test/java/ru/bychkov/patientappointment/PatientAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bychkov.patientappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
